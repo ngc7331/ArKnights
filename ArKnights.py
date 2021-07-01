77f33c44047c834e764a90b50e5b57cf9cfbeb0f
ngc7331 <ngc7331@outlook.com>
2021-07-01 21:58:28 +0800

    0701

--- ArKnights.py
+++ ArKnights.py
@@ -9,53 +9,28 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets, QtGui
 
-class Setting(QWidget):
-    def __init__(self, parent):
+class Ark(QWidget):
+    def __init__(self, conf:dict):
         super().__init__()
-        self.setWindowTitle('Setting')
+        self.conf = conf
+        self.mouse_locked = self.conf['mouse_locked']
+        self.InitWindow()
+        self.InitTrayIcon()
+        self.InitKnights()
+        self._tray_icon.show()
+
+    def InitWindow(self):
+        self.setWindowTitle('ArKnights - 设置')
         self.resize(200, 200)
         self.should_close = False
-        self.btn_test = QPushButton('btn_test')
-        self.btn_test.clicked.connect(parent.Quit)
-        self.btn_close = QPushButton('btn_close')
-        self.btn_close.clicked.connect(self.close)
+        self.btn_close = QPushButton('关闭')
+        self.btn_close.clicked.connect(self.hide)
+        self.btn_test = QPushButton('退出')
+        self.btn_test.clicked.connect(self.Quit)
         layout = QVBoxLayout()
         layout.addWidget(self.btn_test)
         layout.addWidget(self.btn_close)
         self.setLayout(layout)
-    def closeEvent(self, event):
-        if (self.should_close):
-            event.accept()
-        else:
-            event.ignore()
-            self.hide()
-
-class Knight(QWidget):
-    def __init__(self, conf):
-        super().__init__()
-        self.setting = Setting(self)
-
-        self.InitWindow()
-        self.InitTrayIcon()
-        self.InitModelHolder()
-        self.stat_rec = ['Idle', 'Idle']
-        self.InitModel()
-        self.following_mouse = False
-        self.mouse_locked = False
-        self.mouse_pos = self.pos()
-        self.InitTimer()
-        self._tray_icon.show()
-        self.show()
-
-    def InitWindow(self):
-        '''初始化窗口'''
-        self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint|Qt.SubWindow)
-        self.setAutoFillBackground(False)
-        self.setAttribute(Qt.WA_TranslucentBackground, True)
-        self.repaint()
-        # 设置大小和位置
-        self.resize(conf['size'][0], conf['size'][1])
-        self.move(conf['init_pos'][0], conf['init_pos'][1])
 
     def InitTrayIcon(self):
         '''初始化任务栏菜单'''
@@ -63,7 +38,7 @@ class Knight(QWidget):
         tray_icon_menu = QMenu(self)
         obj_list = {
             '鼠标锁定': [None, self.MouseLock],
-            '设置': ['resources/icon/setting.svg', self.setting.show],
+            '设置': ['resources/icon/setting.svg', self.show],
             '重载': ['resources/icon/reload.svg', self.Reload],
             '退出': ['resources/icon/quit.svg', self.Quit]
         }
@@ -74,6 +49,67 @@ class Knight(QWidget):
             tray_icon_menu.addAction(self._menu_action[obj])
         self._tray_icon.setIcon(QIcon('resources/favicon.ico'))
         self._tray_icon.setContextMenu(tray_icon_menu)
+
+    def InitKnights(self):
+        self.knights = []
+        for knight in list(self.conf['knights'].keys()):
+            if (not self.conf['knights'][knight]['enabled']):
+                continue
+            logging.info('Create %s' % knight)
+            self.knights.append(Knight(self, knight))
+
+    def MouseLock(self):
+        if (self.mouse_locked):
+            self._menu_action['鼠标锁定'].setIcon(QIcon())
+        else:
+            self._menu_action['鼠标锁定'].setIcon(QIcon('resources/icon/checked.svg'))
+        self.mouse_locked = not self.mouse_locked
+        for knight in self.knights:
+            knight.mouse_locked = self.mouse_locked
+
+    def Reload(self):
+        for knight in self.knights:
+            logging.info('Stop %s' % knight.name)
+            knight.Quit()
+        self.InitKnights()
+
+    def Quit(self):
+        self.should_close = True
+        for knight in self.knights:
+            knight.close()
+        self.close()
+        sys.exit()
+
+    def closeEvent(self, event):
+        if (self.should_close):
+            event.accept()
+        else:
+            event.ignore()
+            self.hide()
+
+class Knight(QWidget):
+    def __init__(self, ark: Ark, name: str):
+        super().__init__()
+        self.ark, self.name, self.conf, self.fps = ark, name, ark.conf['knights'][name], ark.conf['fps']
+        self.InitWindow()
+        self.InitModelHolder()
+        self.stat_rec = ['Idle', 'Idle']
+        self.InitModel()
+        self.following_mouse = False
+        self.mouse_locked = ark.mouse_locked
+        self.mouse_pos = self.pos()
+        self.InitTimer()
+        self.show()
+
+    def InitWindow(self):
+        '''初始化窗口'''
+        self.setWindowFlags(Qt.FramelessWindowHint|Qt.WindowStaysOnTopHint|Qt.SubWindow)
+        self.setAutoFillBackground(False)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+        self.repaint()
+        # 设置大小和位置
+        self.resize(self.conf['size'][0], self.conf['size'][1])
+        self.move(self.conf['init_pos'][0], self.conf['init_pos'][1])
 
     def InitModelHolder(self):
         self.holder = QLabel(self)
@@ -88,7 +124,6 @@ class Knight(QWidget):
             img = img.scaled(self.size())
             return img
         self.model = {}
-        self.name = conf['model_name']
         root_dir = 'resources/model'
         dir_list = os.listdir(root_dir)
         for dir in dir_list:
@@ -98,9 +133,6 @@ class Knight(QWidget):
             logging.error('resources/model/%s not found, please check!' % self.name)
             logging.warning('Using %s instead.' % dir_list[0])
             self.name = dir_list[0]
-        with open(os.path.join(root_dir, self.name, 'conf.json'), 'r') as f:
-            model_conf = json.load(f)
-            self.model_fps = model_conf['fps']
         img_list = os.listdir(os.path.join(root_dir, self.name))
         for img in img_list:
             if (not img.endswith('.png')):
@@ -109,30 +141,28 @@ class Knight(QWidget):
             if (action not in self.model.keys()):
                 self.model[action] = []
             self.model[action].append(LoadImage(os.path.join(root_dir, self.name, img)))
-        self.stat = list(self.model.keys())[0]
+        self.stat = 'Idle'
         self.i = 0
         self.real_i = 0
-        self.model_len = 0
         self.heading = 0 # 0=Right, 1=Left
         self.playing = False
 
     def InitTimer(self):
         self.timer = QTimer()
         self.timer.timeout.connect(self.RunModel)
-        self.timer.start(1000/conf['fps'])
+        self.timer.start(1000/self.fps)
 
-    def RandomAction(self, hit_wall = False):
+    def RandomAction(self, hit_wall: bool = False):
         self.stat_rec = [self.stat_rec[1], self.stat]
         rand = random.randint(0,100)
-        logging.debug('rand=%s hit_wall=%s' % (rand, hit_wall))
+        logging.debug('%s: rand=%s hit_wall=%s' % (self.name, rand, hit_wall))
         if (rand > 80 or hit_wall or self.stat == 'Click'):
             if (self.stat_rec[0] == 'Idle' and self.stat_rec[1] == 'Move' and hit_wall):
-                logging.debug('Force change status to Move')
+                logging.debug('%s: Force change status to Move' % self.name)
                 self.stat = 'Move'
             else:
-                logging.debug('Randomly change status')
+                logging.debug('%s: Randomly change status' % self.name)
                 self.stat = ['Idle', 'Move'][random.randint(0, 1)]
-            self.model_len = len(self.model[self.stat])
             if (self.stat == 'Move'):
                 if (hit_wall):
                     self.heading = 1 - self.heading
@@ -145,29 +175,21 @@ class Knight(QWidget):
         self.playing = True
 
     def RunModel(self):
-        if (not self.playing or self.i >= self.model_len):
+        if (not self.playing or self.i >= len(self.model[self.stat])):
             self.RandomAction()
         frame = self.model[self.stat][int(self.i)]
-        logging.debug('%5s %5s %5s' % (self.stat, int(self.i), ['Right', 'Left'][self.heading]))
+        logging.debug('%s: %5s %5s %5s' % (self.name, self.stat, int(self.i), ['Right', 'Left'][self.heading]))
         if (self.stat == 'Move'):
             screenRect = QApplication.desktop().screenGeometry()
             x = self.pos().x()
             x += 2 * (0.5-self.heading)
             if ((x<=0 and self.heading == 1) or (x>=screenRect.width()-self.size().width() and self.heading == 0)):
-                logging.info('Hit Wall')
+                logging.info('%s Hit Wall' % self.name)
                 self.RandomAction(hit_wall = True)
                 return None
             self.move(x, self.pos().y())
         self.holder.setPixmap(QPixmap.fromImage(frame.mirrored(self.heading, False)))
-        self.i += self.model_fps/conf['fps']
-
-    def MouseLock(self):
-        if (self.mouse_locked):
-            self.mouse_locked = False
-            self._menu_action['鼠标锁定'].setIcon(QIcon())
-        else:
-            self.mouse_locked = True
-            self._menu_action['鼠标锁定'].setIcon(QIcon('resources/icon/checked.svg'))
+        self.i += self.conf['fps']/self.fps
 
     def mousePressEvent(self, a0: QtGui.QMouseEvent) -> None:
         if (a0.button() == Qt.LeftButton and not self.mouse_locked):
@@ -185,23 +207,13 @@ class Knight(QWidget):
     def mouseDoubleClickEvent(self, a0: QtGui.QMouseEvent) -> None:
         if (a0.button() == Qt.LeftButton):
             if ('Click' not in self.model.keys()):
-                logging.warning('Click anim not found')
+                logging.warning('%s: Click anim not found' % self.name)
                 return None
             self.stat = 'Click'
             self.i = 0
 
-    def Reload(self):
-        self.playing = False
-        self.hide()
-        self.InitModel()
-        self.InitTimer()
-        self.show()
-
     def Quit(self):
-        self.setting.should_close = True
-        self.setting.close()
         self.close()
-        sys.exit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -212,18 +224,17 @@ if __name__ == "__main__":
     except FileNotFoundError:
         screenRect = QApplication.desktop().screenGeometry()
         conf = {
-            'init_pos': [screenRect.width()-256, screenRect.height()-256], #[x, y]
-            'size': [256, 256], #[x, y]
             'fps': 60,
-            'model_name': 'FrostNova'
+            'knights': {
+                'FrostNova': {
+                    'init_pos': [screenRect.width()-256, screenRect.height()-256], #[x, y]
+                    'size': [256, 256] #[x, y]
+                }
+            },
+            'mouse_locked': True
         }
         with open('./conf.json', 'w') as f:
-            f.write(json.dumps(
-                conf,
-                ensure_ascii = False,
-                indent = 4,
-                separators=(',', ': ')
-            ))
+            f.write(json.dumps(conf, ensure_ascii = False, indent = 4, separators=(',', ': ')))
     logging.info(conf)
-    knight = Knight(conf)
+    ark = Ark(conf)
     sys.exit(app.exec_())
